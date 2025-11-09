dda0cb47552e7d87102c3ccbb4aac91be78617b4
Ariff-dev <ariff.dev24@gmail.com>
2025-11-09 03:32:51 -0600

    Adv: Board Controller

--- src/Controllers/BoardController.php
+++ src/Controllers/BoardController.php
@@ -1,16 +1,19 @@
 <?php
 
 require_once __DIR__ . '/../Config/database.php';
+require_once __DIR__ . '/../Models/BoardItem.php';
 require_once __DIR__ . '/../Middlewares/ApiKeyAuth.php';
 class BoardController {
 
     private $db;
     private $conn;
+    private $boardItem;
     private $apiKeyData;
 
     private function __construct(){
         $this->db = new Database();
         $this->conn = $this->db->getConnection();
+        $this->boardItem = new BoardItem($this->conn);
     }
 
     private function authenticate($requiredPermission = null) {
@@ -19,6 +22,31 @@ class BoardController {
             requirePermission($this->apiKeyData, $requiredPermission);
         }
     }
+
+    public function index($id = null) {
+        $this->authenticate('read'); 
+
+        try {
+            $items = $this->boardItem->getAll();
+            http_response_code(200);
+            echo json_encode([
+                'success' => true,
+                'data' => $items,
+                'count' => count($items)
+            ]);
+        } catch (PDOException $e) {
+            http_response_code(500);
+            echo json_encode([
+                'error' => true,
+                'message' => 'Error' . $e->getMessage() 
+            ]);
+        }
+
+    }
+
+
+
+
 
 
 
